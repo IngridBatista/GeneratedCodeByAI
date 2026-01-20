d4046972d98f64e248eac8c1217ce7292eb32e52
fingrid <fingridbatista@gmail.com>
2026-01-20 00:56:39 -0300

    Código gerado pelo GPT

---
+++ GeneratedCodeByAI/GeneratedCode/Gpt/ArrayDifference/Participant_1/GTP_ARRAY_DIFFERENCE_SENIOR_PARTICIPANT_1.cs
@@ -0,0 +1,19 @@
+namespace GTP.ARRAY_DIFFERENCE.SENIOR.PARTICIPANT_1
+{
+    public class GTP_ARRAY_DIFFERENCE_SENIOR_PARTICIPANT_1
+    {
+        public static int[] Diferenca(int[] primeiro, int[] segundo)
+        {
+            if (primeiro == null) throw new ArgumentNullException(nameof(primeiro));
+            if (segundo == null) throw new ArgumentNullException(nameof(segundo));
+
+            // Coloca os elementos do segundo array em um HashSet para busca rápida
+            var conjuntoSegundo = new HashSet<int>(segundo);
+
+            // Filtra apenas os elementos do primeiro que não estão no segundo
+            return primeiro
+                .Where(x => !conjuntoSegundo.Contains(x))
+                .ToArray();
+        }
+    }
+}
